7f1bf5c597b27c53e3fae9ccd6f3493264a32502
gunlords <tur68285@temple.edu>
2024-11-08 09:09:29 -0500

    Countdown add

---
+++ Bassline Burn Game/Assets/Countdown.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountdownController : MonoBehaviour
+{
+    public int countdownTime;
+    public Text countdownDisplay;
+
+    public void Start()
+    {
+        StartCoroutine(CountdownToStart());
+    }
+
+    IEnumerator CountdownToStart()
+    {
+        while (countdownTime > 0)
+        {
+            countdownDisplay.text = countdownTime.ToString();
+            yield return new WaitForSeconds(1f);
+            countdownTime--;
+
+        }
+
+        countdownDisplay.text = "Go!";
+        yield return new WaitForSeconds(1f);
+        countdownDisplay.gameObject.SetActive(false);
+    }
+
+
+}
+
+
