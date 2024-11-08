15d5982db9408cb703484ad8d66f2960fa4c5f3c
tur97019 <tur97019@temple.edu>
2024-11-07 20:03:26 -0500

    changes to see if it works

--- server/mafiaParameters.js
+++ server/mafiaParameters.js
@@ -1,8 +1,8 @@
 const roles = [
-    { name: 'Mafia', description: 'The Mafia’s goal is to eliminate all Citizens without revealing their identity.' },
+    { name: 'Mafia', description: 'The Mafia’s goal is to eliminate all Citizens.' },
     { name: 'Citizen', description: 'Citizens must identify the Mafia members and vote them out.' },
     { name: 'Citizen', description: 'Citizens must identify the Mafia members and vote them out.' },
-    { name: 'Godfather', description: 'The Godfather is the leader of the Mafia and must be the last one standing.' },
+    { name: 'Citizen', description: 'Citizens must identify the Mafia members and vote them out.' },
     { name: 'Citizen', description: 'Citizens must identify the Mafia members and vote them out.' }
 ];
 
