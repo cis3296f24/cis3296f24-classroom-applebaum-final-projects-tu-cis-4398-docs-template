dc1cee30c6010080c0b633db142f76758e1c3769
tur97019 <tur97019@temple.edu>
2024-11-13 16:43:20 -0500

    added voting system

--- server/mafiaParameters.js
+++ server/mafiaParameters.js
@@ -1,9 +1,14 @@
 const roles = [
+    { name: 'Mafia' },
+    { name: 'Citizen' },
+    { name: 'Citizen' },
+    { name: 'Citizen' },
+    { name: 'Citizen' }
+];
+
+const roleDesc = [
     { name: 'Mafia', description: 'The Mafia’s goal is to eliminate all Citizens.' },
-    { name: 'Citizen', description: 'Citizens must identify the Mafia members and vote them out.' },
-    { name: 'Citizen', description: 'Citizens must identify the Mafia members and vote them out.' },
-    { name: 'Citizen', description: 'Citizens must identify the Mafia members and vote them out.' },
     { name: 'Citizen', description: 'Citizens must identify the Mafia members and vote them out.' }
 ];
 
-module.exports = roles;
+module.exports = { roles, roleDesc };
