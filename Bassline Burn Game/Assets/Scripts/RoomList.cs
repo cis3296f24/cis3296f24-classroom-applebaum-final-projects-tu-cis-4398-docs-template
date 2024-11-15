fe0914c686906bcbe73a08635da747240f07c169
Gabriel Lantigua <143743234+glantig1@users.noreply.github.com>
2024-11-14 23:12:36 -0500

    Now vid 4 completely done

--- Bassline Burn Game/Assets/Scripts/RoomList.cs
+++ Bassline Burn Game/Assets/Scripts/RoomList.cs
@@ -8,12 +8,26 @@ using Photon.Realtime;
 public class RoomList : MonoBehaviourPunCallbacks
 {
     public GameObject RoomPrefab;
+    public GameObject[] AllRooms;
     public override void OnRoomListUpdate(List<RoomInfo> roomList)
     {
+        for(int i=0;i<AllRooms.Length; i++){
+            if(AllRooms[i] != null){
+                Destroy(AllRooms[i]);
+            }
+
+        }
+        AllRooms = new GameObject[roomList.Count];
         for(int i=0;i<roomList.Count; i++){
-            print(roomList[i].Name);
-            GameObject Room = Instantiate(RoomPrefab, Vector3.zero, Quaternion.identity, GameObject.Find("Content").transform);
-            Room.GetComponent<Room>().Name.text = roomList[i].Name;
+            if(roomList[i].IsOpen && roomList[i].IsVisible){
+                 print(roomList[i].Name);
+                GameObject Room = Instantiate(RoomPrefab, Vector3.zero, Quaternion.identity, GameObject.Find("Content").transform);
+                Room.GetComponent<Room>().Name.text = roomList[i].Name;
+
+                AllRooms[i] = Room;
+
+            }
+           
         }
     }
 }
