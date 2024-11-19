036e3c93b8bcadae26d916926c836fbeb4076330
JohnDaang <tul16091@temple.edu>
2024-11-19 16:22:30 -0500

    added check for win conditions as well as a basic game over text customized for the players

--- client/src/startGame.js
+++ client/src/startGame.js
@@ -41,24 +41,22 @@ function StartGame() {
           } else if (data.type === 'voteResults') {
               setEliminatedPlayers(prev => [...prev, data.eliminatedPlayer]);                     // adds the eliminated player to the array
               setVoting(false);                                                                   // turns off voting (can be useful for next phase implementation)
-              if (isMafia(data.elimnatedRole)) {
-                setMessages(prev => [...prev, `${data.eliminatedPlayer} has been eliminated! They were a MAFIA!`]);
-              } else {
-                setMessages(prev => [...prev, `${data.eliminatedPlayer} has been eliminated! They were a CITIZEN!`]);
-              }
+              setMessages(prev => [...prev, data.message]); 
               setVotes({});                                                                       // reset vote tally for players
           } else if (data.type === 'voteTie') {
               setVoting(false);                                                                   // turns off voting
               setMessages(prev => [...prev, data.message]);                                       // reset vote tally for players
               setVotes({});
           } else if (data.type === 'NIGHT') {
-            setVoting(false);                                                                     // turns off voting
-            setIsDay(false);                                                                      // sets the page phase to nighttime
-            startTimer(10);                                                                       
+              setVoting(false);                                                                     // turns off voting
+              setIsDay(false);                                                                      // sets the page phase to nighttime
+              startTimer(10);                                                                       
           } else if (data.type === 'DAY') {
-            setVoting(false);                                                                     // turns off voting
-            setIsDay(true); 
-            startTimer(10);                              
+              setVoting(false);                                                                     // turns off voting
+              setIsDay(true); 
+              startTimer(10);                              
+          } else if (data.type === 'gameOver') {
+            setMessages(prev => [...prev, data.message]);
           }
           //setMessages((prevMessages) => [...prevMessages, data.message]); <-- this line was sending duplicate messages to frontend, idk if it is needed or not?
     }
@@ -89,12 +87,6 @@ function StartGame() {
     return () => clearInterval(timer);            // cleanup interval on component unmount or when timer is inactive
   }, [isActive, timeLeft]);
 
-  function isMafia(role) {                                                                                                // function to check if a role is on the Mafia team, can be updated with added roles.
-    if (role === "Mafia") {
-        return true
-    }
-    return false;
-  }
 
   const startTimer = (time) => {
     setTimeLeft(time)
